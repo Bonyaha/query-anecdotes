5c21bce2f829de9dec698ac8a9f5f3dba9b7be8f
Roman <romchikus1987@gmail.com>
2023-04-05 16:47:13 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
+
+import { getAnecdotes, createAnecdote, updateAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    },
+  })
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('anecdotes')
+    },
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery('anecdotes', getAnecdotes, {
+    retry: false,
+  })
+  console.log(result)
+
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const anecdotes = result.data
+  console.log(anecdotes)
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
-      <Notification />
-      <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </div>
-        </div>
+
+      <Notification
+        message={
+          result.error
+            ? 'anecdote service not available due to server problem'
+            : null
+        }
+      />
+      {!result.error && (
+        <>
+          <AnecdoteForm newAnecdoteMutation={newAnecdoteMutation} />
+
+          {anecdotes.map((anecdote) => (
+            <div key={anecdote.id}>
+              <div>{anecdote.content}</div>
+              <div>
+                has {anecdote.votes}
+                <button onClick={() => handleVote(anecdote)}>vote</button>
+              </div>
+            </div>
+          ))}
+        </>
       )}
     </div>
   )
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,18 +1,14 @@
-const Notification = () => {
+const Notification = ({ message }) => {
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 5
+    marginBottom: 5,
   }
-  
-  if (true) return null
 
-  return (
-    <div style={style}>
-      
-    </div>
-  )
+  if (!message) return null
+
+  return <div style={style}>{message}</div>
 }
 
 export default Notification
